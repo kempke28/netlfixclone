b13c32ed2590218639a8abde98145e6057037be9
AndresKempke <kempke@tesolva.de>
2022-08-24 00:48:50 +0200

    Project finished

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -7,7 +7,6 @@ import BrowseContainer from "../containers/browse";
 export default function Browse() {
     const {series} = useContent('series');
     const {films} = useContent('films');
-
     const slides = selectionFilter({series, films});
 
     return (
