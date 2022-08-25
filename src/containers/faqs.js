609edb7a4a8c71f7852c25a88e57d9cc748c4afc
AndresKempke <kempke@tesolva.de>
2022-08-25 10:34:48 +0200

    Adding tests to the project

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,19 +1,20 @@
 import React from "react";
 import faqsData from "../fixtures/faqs.json";
-
-import { Accordion } from "../components";
-import { OptForm } from "../components";
+import {Accordion, OptForm} from "../components";
 
 export function FaqsContainer() {
     return (
         <Accordion>
             <Accordion.Title>Frequently asked questions</Accordion.Title>
+            <Accordion.Frame>
             {faqsData.map((item) => (
-                <Accordion.Item key={item.id}>
-                    <Accordion.Header>{item.header}</Accordion.Header>
-                    <Accordion.Body>{item.body}</Accordion.Body>
-                </Accordion.Item>
-            ))}
+
+                    <Accordion.Item key={item.id}>
+                        <Accordion.Header>{item.header}</Accordion.Header>
+                        <Accordion.Body>{item.body}</Accordion.Body>
+                    </Accordion.Item>
+                    ))}
+            </Accordion.Frame>
 
             <OptForm>
                 <OptForm.Input placeholder="Email address"/>
@@ -22,5 +23,5 @@ export function FaqsContainer() {
                 <OptForm.Text>Create a membership</OptForm.Text>
             </OptForm>
         </Accordion>
-    );
-}
+                );
+            }
